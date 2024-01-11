597d98ac0ccc5e0f9e14a5dd59b50267a2afb832
ESSALHI MUSTAPHA <essalhimu@gmail.com>
2024-01-11 16:29:45 +0000

    fix an issue

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,40 +1,36 @@
 #!/usr/bin/python3
 # distribute an archive to a web server by using fabric.
 from fabric.api import env, put, run
-from os.path import exists
+import os
 
 env.hosts = ["54.165.64.199", "54.237.13.5"]
 
 
 def do_deploy(archive_path):
-    """distributes an archive to your web servers,
-       using the function do_deploy
-    """
-    if not exists(archive_path):
+    """Distributes an archive to a web server."""
+
+    if not os.path.isfile(archive_path):
         return False
+
+    file_name = os.path.basename(archive_path)
+    name = os.path.splitext(file_name)[0]
+
     try:
-        file_name = archive_path.split("/")[-1].split(".")[0]
-        put(archive_path, "/tmp/")
-
-        run("mkdir -p /data/web_static/releases/{}".format(file_name))
-
-        run("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/"
-            .format(file_name, file_name))
-
-        run('rm -rf /tmp/{}.tgz'.format(file_name))
-
-        run(('mv /data/web_static/releases/{}/web_static/* ' +
-            '/data/web_static/releases/{}/')
-            .format(file_name, file_name))
-
-        run('rm -rf /data/web_static/releases/{}/web_static'
-            .format(file_name))
-
-        run('rm -rf /data/web_static/current')
-
-        run(('ln -s /data/web_static/releases/{}/' +
-            ' /data/web_static/current')
-            .format(file_name))
+        put(archive_path, "/tmp/{}".format(file_name))
+        run("rm -rf /data/web_static/releases/{}/".format(name))
+        run("mkdir -p /data/web_static/releases/{}/".
+            format(name))
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+            format(file_name, name))
+        run("rm /tmp/{}".format(file_name))
+        run("mv /data/web_static/releases/{}/web_static/*"
+            "/data/web_static/releases/{}/".
+            format(name, name))
+        run("rm -rf /data/web_static/releases/{}/web_static".format(name))
+        run("rm -rf /data/web_static/current")
+        run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+            format(name))
         return True
-    except Exception:
+    except Exception as e:
+        print(f"Error: {e}")
         return False
