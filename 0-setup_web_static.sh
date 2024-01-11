5c2334b8fe5e176c3e5e18dc94fefd179a09f348
ESSALHI MUSTAPHA <essalhimu@gmail.com>
2024-01-11 12:32:48 +0000

    install and configure nginx script

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+# install & configure nginx
+
+apt-get update
+apt-get install -y nginx
+
+wb_path="/data/web_static"
+mkdir -p "$wb_path/releases/test/"
+mkdir -p "$wb_path/shared/"
+echo "Holberton School" > "$wb_path/releases/test/index.html"
+ln -sf "$wb_path/releases/test/" "$wb_path/current"
+
+chown -R ubuntu "$wb_path"
+chgrp -R ubuntu "$wb_path"
+
+nginx_config="/etc/nginx/sites-available/default"
+cat > "$nginx_config" <<EOF
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By \$HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
+
+    location /hbnb_static {
+        alias $web_static_path/current;
+        index index.html index.htm;
+    }
+
+    location /redirect_me {
+        return 301 http://cuberule.com/;
+    }
+
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}
+EOF
+
+service nginx restart
