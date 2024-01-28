f748d8e3ab23c4bfd1b42690d32a37858521f3fa
ESSALHI MUSTAPHA <essalhimu@gmail.com>
2024-01-28 19:31:17 +0000

    change some lines in states

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -2,15 +2,14 @@
 """Script that starts a Flask web application"""
 from flask import Flask, render_template
 from models import storage
-from models.state import State
 
 app = Flask(__name__)
 
 
-@app.before_request
-def before_request():
-    """Run before each request"""
-    storage.reload()
+# @app.before_request
+# def before_request():
+#     """Run before each request"""
+#     storage.reload()
 
 
 @app.route("/states", strict_slashes=False)
